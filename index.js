4224fae58faa4f916e8fc23f603e3b3300a62db9
cordeirocCaio <caioc.cordeiro@hotmail.com>
2024-07-21 10:27:25 -0300

    Atualizar o código da função calcularRankeadas

---
+++ index.js
@@ -0,0 +1,24 @@
+function calcularRankeadas(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 e vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+    return "O Herói tem de saldo " + saldoVitorias + " e está no nível " + nivel;
+}
+
+console.log(calcularRankeadas(15, 5));  
